2acdb916a34c1722a3810c178d53eb855eed32a6
Akshit-py <58499165+Akshit-py@users.noreply.github.com>
2026-07-08 20:44:54 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,9 @@
+#import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        '''rs=re.sub('[^a-zA-Z0-9]','',s).lower()
+        if rs==rs[::-1]:
+            return True
+        return False'''
+        rs=[i.lower() for i in s if i.isalnum()]
+        return rs==rs[::-1]
