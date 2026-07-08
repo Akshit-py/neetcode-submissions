398451aa0457c726b2e8e5630c7e99cac59952e5
Akshit-py <58499165+Akshit-py@users.noreply.github.com>
2026-07-08 21:49:32 +0530

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,24 @@
+import itertools
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        fl=[]
+        for i in range(0,len(numbers)):
+            comp=target-numbers[i]
+            if comp in numbers[i+1:]:
+                fl.append([numbers[i],comp])
+        return fl
+    
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        finalList=[]
+        for i in range(0,len(nums)):
+            comp=0-nums[i]
+            imList=self.twoSum(nums[i+1:],comp)
+            if len(imList)>0:
+                for j in imList:
+                    finalList.append(sorted([nums[i],j[0],j[1]]))
+        finalList.sort()
+        j=list(k for k,_ in itertools.groupby(finalList))
+        return j
+
+
+        
